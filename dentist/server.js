0ec44728b1ab87511fae797375e1719b5e78947e
Bartek <gbartek123@gmail.com>
2022-12-06 13:03:28 +0100

    test4

--- dentist/server.js
+++ dentist/server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Worldbee\n');
+    res.end('Hello Worldbeeeeee\n');
 }).listen(port);
