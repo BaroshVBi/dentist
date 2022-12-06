11a06285f9a380ea1388c1c9d0c40c3a212b5755
Bartek <gbartek123@gmail.com>
2022-12-06 14:35:51 +0100

    fs

--- dentist/server.js
+++ dentist/server.js
@@ -1,8 +1,14 @@
 'use strict';
 var http = require('http');
+var fs = require('fs');
 var port = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Worldbeeeeee\n');
+    //res.end('Hello Worldbeeeeee\n');
+    fs.readFile('Page1.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
 }).listen(port);
