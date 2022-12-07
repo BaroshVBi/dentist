53ef0e2fb0c150a89c36b961bf19bb10e4bf8fa7
Bartek <gbartek123@gmail.com>
2022-12-07 10:22:19 +0100

    express

--- dentist/server.js
+++ dentist/server.js
@@ -1,14 +1,18 @@
 'use strict';
-var http = require('http');
+var path = require('path');
+var express = require('express');
+var app = require('express')();
+var http = require('http').createServer(app);
 var fs = require('fs');
 var port = process.env.PORT || 8080;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    //res.end('Hello Worldbeeeeee\n');
-    fs.readFile('Page1.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
-}).listen(port);
+app.use(express.static(path.join(__dirname, '/Public')));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/Public/Page1.html');
+});
+
+http.listen(port, () => {
+});
+
+
