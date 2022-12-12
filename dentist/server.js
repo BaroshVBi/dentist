b6c00918805ab5e6b58bdec15a1651ea81674240
Bartek <gbartek123@gmail.com>
2022-12-12 10:47:57 +0100

    body parser

--- dentist/server.js
+++ dentist/server.js
@@ -5,14 +5,25 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var fs = require('fs');
 var cookie = require('cookie-parser');
+var bodyParser = require('body-parser');
 var io = require('socket.io')(http);
+
 var port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '/Public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/Public/Page1.html');
 });
+
+app.post('/rezerwacja', (req, res) => {
+	res.sendFile(__dirname + '/Public/Page1.html');
+	console.log("submit");
+	console.log(req.body.godzina);
+});
+
 
 http.listen(port, () => {
 
