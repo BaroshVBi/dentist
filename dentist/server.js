efbc1da3c5156d6404cf6dee87e8432a18e09712
Bartek <gbartek123@gmail.com>
2022-12-12 12:50:05 +0100

    cookie

--- dentist/server.js
+++ dentist/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').createServer(app);
 var fs = require('fs');
-var cookie = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var io = require('socket.io')(http);
 
@@ -12,16 +12,21 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '/Public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+var test = {
+	godzina : "3"
+}
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/Public/Page1.html');
 });
 
 app.post('/rezerwacja', (req, res) => {
 	res.sendFile(__dirname + '/Public/Page2.html');
+	res.cookie('godzina', req.body.godzina);
 	console.log("submit");
-	console.log(req.body.godzina);
+	console.log(req.body);
 });
 
 
